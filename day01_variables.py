103035f211eeb783da9afff9cfc0f16cdc188b25
Sachin Dangi <sachindangii2026@gmail.com>
2026-03-16 21:19:10 +0530

    Create day01_variables.py

---
+++ day01_variables.py
@@ -0,0 +1,39 @@
+# =============================
+# Day 01 - Variables & Data Types
+# Date: 16 March 2026
+# =============================
+
+# --- String ---
+name = "Sachin Dangi"
+language = "Python"
+print(f"Name: {name}")
+print(f"Learning: {language}")
+
+# --- Integer ---
+age = 17
+day_number = 1
+print(f"Age: {age}")
+print(f"Day: {day_number}")
+
+# --- Float ---
+version = 3.11
+print(f"Python Version: {version}")
+
+# --- Boolean ---
+is_learning = True
+is_expert = False
+print(f"Am I learning? {is_learning}")
+print(f"Am I expert yet? {is_expert}")
+
+# --- List ---
+skills = ["Python", "Git", "GitHub"]
+print(f"My Skills: {skills}")
+
+# --- Dictionary ---
+profile = {
+    "name": "Sachin Dangi",
+    "age": 17,
+    "goal": "AI/ML Developer",
+    "day": 1
+}
+print(f"Profile: {profile}")
